233f2810ce2deda949d8e315b72dbaa9b7fb1615
kefeletswe <kefeletswenonhlanhla@gmail.com>
2022-11-04 00:18:50 +0000

    add lists of lists possible answers

--- run.py
+++ run.py
@@ -13,5 +13,9 @@ questions = {
  "What does a broken yellow line mean?: ": "C",
  "What does a single continuous yellow line mean??: ": "A"
 }
+options = [["A.  When the driver in front of you is turning right", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+          ["A. 1989", "B.  You’re meeting on-coming traffic", "C. 2000", "D. 2016"],
+          ["A. Lonely Island", "B. Smosh", "C. A broken yellow line signifies the edge of the roadway", "D. SNL"],
+          ["A. A single yellow line signifies parking is prohibited at certain times","B. False", "C. sometimes", "D. What's Earth?"]]
 
 
