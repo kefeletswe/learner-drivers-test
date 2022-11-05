89f384d6d6948329de36c12090b30352bfe8796c
kefeletswe <kefeletswenonhlanhla@gmail.com>
2022-11-05 11:17:45 +0000

    remove space holders and add string answers on options

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
-
+import gspread
+google.oauth2.service_account import Credentials
 #welcome the user
 print("Welcome to Learners Drivers Test! ")
 username = input("Username:\n")
 print("hey", username,"you need to get all quesntions/Begin Test!")
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
 
 def new_game():
 
