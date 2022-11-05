310560916aae19f34a80e04f0c7fc074470c67c2
kefeletswe <kefeletswenonhlanhla@gmail.com>
2022-11-05 21:25:52 +0000

    fix problems with code

--- run.py
+++ run.py
@@ -1,20 +1,11 @@
-import gspread
-google.oauth2.service_account import Credentials
-#welcome the user
+# welcome user
 print("Welcome to Learners Drivers Test! ")
-username = input("Username:\n")
-print("hey", username,"you need to get all quesntions/Begin Test!")
+username = input("Enter your name?: ")
+id_number = input("Enter your ID number")
+email = input("Enter your email address")
+print("hey", username, " you need to get all quesntions/Begin Test!")
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-
-
+# guesses
 def new_game():
 
     guesses = []
@@ -34,9 +25,8 @@ def new_game():
 
     display_score(correct_guesses, guesses)
 
-
+# answers
 def check_answer(answer, guess):
-
     if answer == guess:
         print("CORRECT!")
         return 1
@@ -44,11 +34,10 @@ def check_answer(answer, guess):
         print("WRONG!")
         return 0
 
-
+# scores
 def display_score(correct_guesses, guesses):
-    print("-------------------------")
     print("RESULTS")
-    print("-------------------------")
+    
 
     print("Answers: ", end="")
     for i in questions:
@@ -63,10 +52,10 @@ def display_score(correct_guesses, guesses):
     score = int((correct_guesses/len(questions))*100)
     print("Your score is: "+str(score)+"%")
 
-
+# ask user if they want to restart test
 def play_again():
 
-    response = input("Do you want to play again? (yes or no): ")
+    response = input("Do you want to start again? (yes or no): ")
     response = response.upper()
 
     if response == "YES":
@@ -76,21 +65,23 @@ def play_again():
 
 
 
-
 questions = {
- "When can you overtake on the left?: ": "A",
- "When driving at night when should you dip your headlights?: ": "B",
- "What does a broken yellow line mean?: ": "C",
- "What does a single continuous yellow line mean??: ": "A"
+ "What is the lagal age to drive a car?: ": "A",
+ "Are you allowed to overtake?: ": "B",
+ "How much is the penalty for overspeeding?: ": "C",
+ "How many passengers are allowed in a car?: ": "A"
 }
-options = [["A.  When the driver in front of you is turning right", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
-          ["A. 1989", "B.  You’re meeting on-coming traffic", "C. 2000", "D. 2016"],
-          ["A. Lonely Island", "B. Smosh", "C. A broken yellow line signifies the edge of the roadway", "D. SNL"],
-          ["A. A single yellow line signifies parking is prohibited at certain times","B. False", "C. sometimes", "D. What's Earth?"]]
+
+options = [["A. 18", "B. 21", "C. 16", "D. 18 and 21"],
+          ["A. Yes", "B. No", "C. Maybe", "D. Someimes"],
+          ["A. 1000", "B. 2000", "C. 3000", "4000"],
+          ["A. 8","B. 9", "C. 10", "D. 2"]]
 
 new_game()
 
 while play_again():
     new_game()
 
-print("See you soon!")
+print("Thank you!")
+
+# end
